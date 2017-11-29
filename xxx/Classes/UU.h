cd41b078e0a81a4355cacc9f0b97cd0da4e65514
huangqg3 <huangqiguo@zyy.local>
2017-11-29 11:11:04 +0800

    no message

---
+++ xxx/Classes/UU.h
@@ -0,0 +1,12 @@
+//
+//  UU.h
+//  Expecta
+//
+//  Created by huangqiguo on 2017/11/29.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UU : NSObject
+
+@end
---
+++ xxx/Classes/UU.m
@@ -0,0 +1,12 @@
+//
+//  UU.m
+//  Expecta
+//
+//  Created by huangqiguo on 2017/11/29.
+//
+
+#import "UU.h"
+
+@implementation UU
+
+@end
