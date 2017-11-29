3c8591014580f01c77c52967f26f2b517323ff50
huangqg3 <huangqiguo@zyy.local>
2017-11-29 10:31:22 +0800

    no message

---
+++ xxx/Classes/test1.h
@@ -0,0 +1,12 @@
+//
+//  test1.h
+//  Expecta
+//
+//  Created by huangqiguo on 2017/11/29.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test1 : NSObject
+
+@end
---
+++ xxx/Classes/test1.m
@@ -0,0 +1,12 @@
+//
+//  test1.m
+//  Expecta
+//
+//  Created by huangqiguo on 2017/11/29.
+//
+
+#import "test1.h"
+
+@implementation test1
+
+@end
