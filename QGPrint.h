73983af967bf6a02160e0e6890ca0c294766979b
huangqg3 <huangqiguo@zyy.local>
2017-11-29 10:12:03 +0800

    1

---
+++ QGPrint.h
@@ -0,0 +1,12 @@
+//
+//  QGPrint.h
+//  Expecta
+//
+//  Created by huangqiguo on 2017/11/29.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QGPrint : NSObject
++(void)printWithStr:(NSString *)str;
+@end
---
+++ QGPrint.m
@@ -0,0 +1,14 @@
+//
+//  QGPrint.m
+//  Expecta
+//
+//  Created by huangqiguo on 2017/11/29.
+//
+
+#import "QGPrint.h"
+
+@implementation QGPrint
++(void)printWithStr:(NSString *)str{
+    NSLog(@"%@",str);
+}
+@end
